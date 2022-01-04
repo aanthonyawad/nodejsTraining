7eeda858aac646820cbdb304e3fede14da82157e
aanthonyawad <dev.anthony.awad@gmail.com>
2022-01-04 14:17:00 +0200

    template starter

--- pocoTraining/nodeFarm/index.js
+++ pocoTraining/nodeFarm/index.js
@@ -1,14 +1,28 @@
+
+// IMPORTS
 import http from "http";
 import url from 'url';
+import fs from 'fs';
 
+//DIRNMAE ALT
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const  data= fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
+const dataObj = JSON.parse(data);
 const server = http.createServer((req,res)=>{
     const pathName = req.url;
 
     if(pathName === '/' || pathName === '/overview'){
 
         return res.end('Hello from Overview');
-    }else if (pathName === '/product'){
+    }else if (pathName === '/product') {
         return res.end('Hello from Product');
+    }else if(pathName === '/api'){
+        res.writeHead(200,{'Content-Type':'application/json'});
+        return res.end(data);
     }else{
         res.writeHead(404);
         return res.end('Page Not found');
