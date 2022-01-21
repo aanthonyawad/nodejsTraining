d582e101ca74bb8268ea72ea8c4c00d406d56344
aanthonyawad <dev.anthony.awad@gmail.com>
2022-01-21 15:51:19 +0200

    create integration tests for the tour controller

--- pocoTraining/natours/test/controllers/tourController.test.js
+++ pocoTraining/natours/test/controllers/tourController.test.js
@@ -182,29 +182,70 @@
 const request = require('supertest');
 const server = require('../../server');
 
+jest.setTimeout(10 * 1000);
+let jwt;
+let tourId;
 describe('Should create app ', () => {
-  let jwt;
   beforeAll(() => {
     process.env.JWT_SECRET = 'natours_secret_jwt';
     process.env.DATABASE = 'mongodb://localhost:27017/natours';
   });
 
   describe('should login', () => {
-    it('validate login', async () => {
+    beforeAll(async function () {
+      // get the token
       const response = await request(server)
         .post(`/api/v1/users/login/`)
         .send({ email: 'anthony123@mailsac.com', password: 'pass1234' });
       expect(response.body.status).toBe('success');
       expect(response.body.token).toBeDefined();
-      jwt = response.token;
+      jwt = response.body.token;
     });
-  });
 
-  describe('Tour Functions', () => {
-    it('GET /api/v1/tours', async () => {
-      const response = await request(server).get(`/api/v1/tours/`);
-      expect(response.body.status).toBe('success');
-      expect(response.body.data.data[0].id).toBe('5c88fa8cf4afda39709c2955');
+    describe('Tour Functions', () => {
+      it('GET /api/v1/tours', async function () {
+        const response = await request(server).get(`/api/v1/tours/`);
+        expect(response.body.status).toBe('success');
+      });
+
+      it('POST /api/v1/tours', async function () {
+        const response = await request(server)
+          .post(`/api/v1/tours/`)
+          .set(
+            //admin Bearer token
+            'Authorization',
+            `Bearer ${jwt}`
+          )
+          .send({
+            name: 'New Test Tour Integration',
+            duration: 1,
+            maxGroupSize: 1,
+            difficulty: 'easy',
+            price: 200,
+            summary: 'Test tour',
+            imageCover: 'tour-3-cover.jpg',
+            startLocation: {
+              description: 'Miami, USA',
+              type: 'Point',
+              coordinates: [-80.185942, 25.774772],
+              address: '301 Biscayne Blvd, Miami, FL 33132, USA',
+            },
+          });
+        expect(response.body.status).toBe('success');
+        tourId = response.body.data.data.id;
+      });
+
+      afterAll(async function () {
+        //delete the newly created testing tour
+        const response = await request(server)
+          .delete(`/api/v1/tours/${tourId}`)
+          .set(
+            //admin Bearer token
+            'Authorization',
+            `Bearer ${jwt}`
+          );
+        expect(response.statusCode).toBe(204);
+      });
     });
   });
 });
